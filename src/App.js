482ea0cfa8b8dc672737ff366424b6636ddf474f
Maksim <maxikpetlitskii@gmail.com>
2024-03-26 00:41:03 +0100

    Refactoring download Route in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 // src/App.js
-
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './redux/store';
 import FileRegistry from './components/FileRegistry';
-import FileUpload from './components/FileUpload';
 import FileDownload from './components/FileDownload';
 
 const App = () => {
@@ -15,8 +13,8 @@ const App = () => {
                 <div>
                     <Routes>
                         <Route path="/" element={<FileRegistry/>}/>
-                        <Route path="/upload" element={<FileUpload/>}/>
                         <Route path="/download" element={<FileDownload/>}/>
+                        <Route path="/download/:cid" element={<FileDownload/>}/>
                     </Routes>
                 </div>
             </Router>
